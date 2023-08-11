03c8db7544f5eb37ecb58bb513afb68d49c10324
TecGabriel-Silva <Tec.gabriel2003@gmail.com>
2023-08-11 20:11:41 -0300

    Version 0.02 - Correção de erros

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :producaos
-  root :to => "producaos#index"
-  #root 'producaos#index' 
+  #root :to => "producaos#index"
+  root 'producaos#index' 
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
